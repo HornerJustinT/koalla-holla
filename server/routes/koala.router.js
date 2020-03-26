3ba37d4752edb0cd92f57c0b2f1632a5aba487e6
Max Faust <tcthinker@gmail.com>
2020-03-26 13:55:21 -0500

    delete feature

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -13,6 +13,23 @@ const koalaRouter = express.Router();
 // PUT
 
 
-// DELETE
+//DELETE - removes a koala
+router.delete('/:id', (req, res) => {
+    // Need to know which koala we are deleting
+    // Instead of using req.body for info, use req.params
+    let koalaId = req.params.id
+    console.log('In delete koala', koalaId);
+  
+    //Use SQL Param to substitute in the koalaID
+    let sqlText = `DELETE FROM "koala" WHERE "id" = $1;`
+    pool.query(sqlText, [koalaId])
+      .then( result => {
+        res.sendStatus(200);
+      })
+      .catch( (error) => {
+        console.log('Error in delete by ID', error);
+        res.sendStatus(500);
+      })
+  });//end DELETE
 
 module.exports = koalaRouter;
