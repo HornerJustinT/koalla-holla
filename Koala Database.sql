8eaee77e47252e515446f9cb638f70c3bed71f0f
Erick Jensen <Erick.Jensen010@gmail.com>
2020-03-26 16:34:41 -0500

    add database entries

--- Koala Database.sql
+++ Koala Database.sql
@@ -6,3 +6,18 @@ CREATE TABLE "koala" (
 "age" INTEGER NOT NULL,
 "ready_to_transfer" varchar(1) NOT NULL,
 "notes" varchar(240));
+
+INSERT INTO "koala" ("name", "gender", "age", "ready_to_transfer", "notes") VALUES ('Scotty', 'M', '4', 'Y', 'Born in Guatemala');
+
+INSERT INTO "koala" ("name", "gender", "age", "ready_to_transfer", "notes") VALUES ('Jean', 'F', '5', 'Y', 'Allergic to lots of lava');
+
+INSERT INTO "koala" ("name", "gender", "age", "ready_to_transfer", "notes") VALUES ('Ororo', 'F', '7', 'N', 'Loves listening to Paula (Abdul)');
+
+INSERT INTO "koala" ("name", "gender", "age", "ready_to_transfer", "notes") VALUES ('Logan', 'M', '15', 'N', 'Loves the sauna');
+
+INSERT INTO "koala" ("name", "gender", "age", "ready_to_transfer", "notes") VALUES ('Charlie', 'M', '9', 'Y', 'Favorite band is Nirvana');
+
+INSERT INTO "koala" ("name", "gender", "age", "ready_to_transfer", "notes") VALUES ('Betsy', 'F', '4', 'Y', 'Has a pet iguana');
+
+SELECT * FROM "koala";
+
